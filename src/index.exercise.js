a8372e4156352087ca28ef5a4ef7493a5c51d0c0
Fenix4088 <fenix4088@gmail.com>
2022-03-13 18:23:43 +0200

    01-bootstrap: 03 Create a LoginForm component

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -12,25 +12,65 @@ export const App = () => {
   const login = () => setOpenModal('login')
   const register = () => setOpenModal('register')
 
+  const handleSubmit = (formData) => {
+    console.log(formData)
+  }
+
   return (
     <>
-      <Dialog aria-label="Login form" isOpen={openModal === 'login'} onDismiss={() => setOpenModal('none')}>
+      <Dialog aria-label='Login form' isOpen={openModal === 'login'} onDismiss={() => setOpenModal('none')}>
         <div>
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Login</h3>
+        <LoginForm buttonText={'Login'} onSubmit={handleSubmit} />
       </Dialog>
-      <Dialog aria-label="Registration form" isOpen={openModal === 'register'} onDismiss={() => setOpenModal('none')}>
+      <Dialog aria-label='Registration form' isOpen={openModal === 'register'} onDismiss={() => setOpenModal('none')}>
         <div>
           <button onClick={() => setOpenModal('none')}>Close</button>
         </div>
         <h3>Register</h3>
+        <LoginForm buttonText={'Register'} onSubmit={handleSubmit} />
       </Dialog>
       <Logo />
       <h1>Bookshelf</h1>
       <button onClick={login}>Login</button>
       <button onClick={register}>Register</button>
     </>
+  )
+}
+
+
+export const LoginForm = ({onSubmit, buttonText}) => {
+  const [formData, setFormData] = React.useState({
+    username: '',
+    password: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(formData)
+  }
+
+  const onUsernameChange = ({currentTarget: {value}}) => {
+    setFormData(fD => ({...fD, username: value}))
+  }
+  const onPasswordChange = ({currentTarget: {value}}) => {
+    setFormData(fD => ({...fD, password: value}))
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input value={formData.username} type='text' name={'username'}
+               onChange={onUsernameChange} />
+      </label>
+      <label>
+        <input value={formData.password} type='password' name={'username'}
+               onChange={onPasswordChange} />
+      </label>
+      <button>{buttonText}</button>
+    </form>
   )
 }
 

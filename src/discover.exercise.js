5cfe43205f06c26a36ed59c985d64e162b762924
Fenix4088 <fenix4088@gmail.com>
2022-03-21 19:10:05 +0200

    03 Make HTTP Requests: 01

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -2,79 +2,83 @@
 import {jsx} from '@emotion/core'
 
 import './bootstrap'
+import React from 'react';
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+    const [{status, data, query}, setQueryState] = React.useState({
+        status: 'idle', // 'idle', 'loading', or 'success'
+        data: null,
+        query: ''
+    });
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+    const [queried, setQueried] = React.useState(false);
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+    const isLoading = status === 'loading';
+    const isSuccess = status === 'success';
 
-  function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
-  }
+    React.useEffect(() => {
+        if (!queried) return;
 
-  return (
-    <div
-      css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
-    >
-      <form onSubmit={handleSearchSubmit}>
-        <Input
-          placeholder="Search books..."
-          id="search"
-          css={{width: '100%'}}
-        />
-        <Tooltip label="Search Books">
-          <label htmlFor="search">
-            <button
-              type="submit"
-              css={{
-                border: '0',
-                position: 'relative',
-                marginLeft: '-35px',
-                background: 'transparent',
-              }}
-            >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
-            </button>
-          </label>
-        </Tooltip>
-      </form>
+        setQueryState(s => ({...s, status: 'loading'}));
 
-      {isSuccess ? (
-        data?.books?.length ? (
-          <BookListUL css={{marginTop: 20}}>
-            {data.books.map(book => (
-              <li key={book.id} aria-label={book.title}>
-                <BookRow key={book.id} book={book} />
-              </li>
-            ))}
-          </BookListUL>
-        ) : (
-          <p>No books found. Try another search.</p>
-        )
-      ) : null}
-    </div>
-  )
+        client(`books?query=${encodeURIComponent(query)}`).then(data => {
+            setQueryState(s => ({...s, status: 'success', data}));
+        })
+    }, [query, queried])
+
+    function handleSearchSubmit(event) {
+        event.preventDefault();
+        setQueried(true);
+        setQueryState(s => ({...s, query: event.target.elements.search.value}));
+    }
+
+    return (
+        <div
+            css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
+        >
+            <form onSubmit={handleSearchSubmit}>
+                <Input
+                    placeholder="Search books..."
+                    id="search"
+                    css={{width: '100%'}}
+                />
+                <Tooltip label="Search Books">
+                    <label htmlFor="search">
+                        <button
+                            type="submit"
+                            css={{
+                                border: '0',
+                                position: 'relative',
+                                marginLeft: '-35px',
+                                background: 'transparent',
+                            }}
+                        >
+                            {isLoading ? <Spinner/> : <FaSearch aria-label="search"/>}
+                        </button>
+                    </label>
+                </Tooltip>
+            </form>
+
+            {isSuccess ? (
+                data?.books?.length ? (
+                    <BookListUL css={{marginTop: 20}}>
+                        {data.books.map(book => (
+                            <li key={book.id} aria-label={book.title}>
+                                <BookRow key={book.id} book={book}/>
+                            </li>
+                        ))}
+                    </BookListUL>
+                ) : (
+                    <p>No books found. Try another search.</p>
+                )
+            ) : null}
+        </div>
+    )
 }
 
 export {DiscoverBooksScreen}

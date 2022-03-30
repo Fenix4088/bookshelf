b1f92a5f41f46da53a76f38653cb2f797685a651
Fenix4088 <fenix4088@gmail.com>
2022-03-30 19:44:35 +0300

    04 Authentication: 04 Support posting data

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,44 +3,77 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {FullPageSpinner} from './components/lib'
+import * as colors from './styles/colors'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
-import {client} from "./utils/api-client.exercise";
 
-export const getUser = async () => {
-    let user = null;
+async function getUser() {
+    let user = null
+
     const token = await auth.getToken()
-
-    if(token) {
+    if (token) {
         const data = await client('me', {token})
-        user = data.user;
+        user = data.user
     }
 
     return user
-
 }
 
 function App() {
-    const [user, setUser] = React.useState(null);
+    const {
+        data: user,
+        error,
+        isLoading,
+        isIdle,
+        isError,
+        isSuccess,
+        run,
+        setData,
+    } = useAsync()
 
     React.useEffect(() => {
-        getUser().then(setUser);
-    }, [])
+        run(getUser())
+    }, [run])
 
-    const login = ({username, password}) => auth.login({username, password}).then(setUser);
-    const registration = ({username, password}) => auth.register({username, password}).then(setUser);
+    const login = form => auth.login(form).then(user => setData(user))
+    const register = form => auth.register(form).then(user => setData(user))
     const logout = () => {
-        auth.logout().then(() => setUser(null))
+        auth.logout()
+        setData(null)
     }
 
+    if (isLoading || isIdle) {
+        return <FullPageSpinner/>
+    }
 
-    return user ? <AuthenticatedApp logout={logout} user={user}/> :
-        <UnauthenticatedApp login={login} register={registration}/>
+    if (isError) {
+        return (
+            <div
+                css={{
+                    color: colors.danger,
+                    height: '100vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <p>Uh oh... There's a problem. Try refreshing the app.</p>
+                <pre>{error.message}</pre>
+            </div>
+        )
+    }
+
+    if (isSuccess) {
+        return user ? (
+            <AuthenticatedApp user={user} logout={logout}/>
+        ) : (
+            <UnauthenticatedApp login={login} register={register}/>
+        )
+    }
 }
 
 export {App}
-
-/*
-eslint
-  no-unused-vars: "off",
-*/

2e99019424a96a0f40f2be0be12a3c200ee524b5
Fenix4088 <fenix4088@gmail.com>
2022-04-24 18:27:29 +0300

    Unit tests: excercise

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,7 +1,7 @@
 import {formatDate} from "../misc";
 
 test('formatDate formats the date to look nice', () => {
-    const [y, m, d] = [2020, 12, 10];
+    const [y, m, d] = [2020, 11, 10];
     const expectedResult = 'Dec 20'
 
     const date = formatDate(new Date(y, m, d))

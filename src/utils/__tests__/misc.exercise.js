634bb00e7daf6ef33199a945a08df96dd7eeb7c5
Fenix4088 <fenix4088@gmail.com>
2022-04-24 10:08:42 +0300

    Unit tests: excercise 1/2

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,12 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from "../misc";
+
+test('formatDate formats the date to look nice', () => {
+    const [y, m, d] = [2020, 12, 10];
+    const expectedResult = 'Dec 20'
+
+    const date = formatDate(new Date(y, m, d))
+
+    expect(typeof date).toBe('string')
+    expect(date).toStrictEqual(expectedResult);
+})
 

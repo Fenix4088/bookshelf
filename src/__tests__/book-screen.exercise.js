fadbdc669af1f46ad13d7873498479d5ef489ef1
Fenix4088 <fenix4088@gmail.com>
2022-04-27 19:48:50 +0300

    Integration tests: extra-1

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -1,53 +1,60 @@
 import * as React from 'react'
-import {
-  render,
-  screen,
-  waitFor,
-  waitForElementToBeRemoved,
-} from '@testing-library/react'
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import {queryCache} from 'react-query'
 import {buildUser, buildBook} from 'test/generate'
 import * as auth from 'auth-provider'
 import {AppProviders} from 'context'
 import {App} from 'app'
 import {localStorageKey} from 'auth-provider'
+import * as usersDB from 'test/data/users'
+import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
 
 // 🐨 after each test, clear the queryCache and auth.logout
-
 afterEach(async () => {
   queryCache.clear()
-  await auth.logout()
+  await Promise.all([
+    auth.logout(),
+    usersDB.reset(),
+    booksDB.reset(),
+    listItemsDB.reset(),
+  ])
 })
 
 test('renders all the book information', async () => {
-  localStorage.setItem(localStorageKey, 'SOME FAKE TOKEN')
-
   const user = buildUser()
-  const book = buildBook()
+  await usersDB.create(user)
+  const authUser = await usersDB.authenticate(user)
+  localStorage.setItem(localStorageKey, authUser.token)
+
+  const book = await booksDB.create(buildBook())
 
   const route = `/book/${book.id}`
   window.history.pushState({}, 'Test page', route)
 
-  const originalFetch = window.fetch
-  window.fetch = async (url, config) => {
-    console.log(url)
-    if (url.endsWith('/bootstrap')) {
-      return {
-        ok: true,
-        json: async () => ({
-          user: {...user, token: 'SOME_FAKE_TOKEN'},
-          listItems: [],
-        }),
-      }
-    } else if (url.endsWith(`/books/${book.id}`)) {
-      return {ok: true, json: async () => ({book})}
-    }
-    return originalFetch(url, config)
-  }
+  // const originalFetch = window.fetch
+  // window.fetch = async (url, config) => {
+  //   console.log(url)
+  //   if (url.endsWith('/bootstrap')) {
+  //     return {
+  //       ok: true,
+  //       json: async () => ({
+  //         user: {...user, token: 'SOME_FAKE_TOKEN'},
+  //         listItems: [],
+  //       }),
+  //     }
+  //   } else if (url.endsWith(`/books/${book.id}`)) {
+  //     return {ok: true, json: async () => ({book})}
+  //   }
+  //   return originalFetch(url, config)
+  // }
 
   const {debug} = render(<App />, {wrapper: AppProviders})
 
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+  await waitForElementToBeRemoved(() => [
+    ...screen.queryAllByLabelText(/loading/i),
+    ...screen.queryAllByText(/loading/i),
+  ])
 
   // debug();
   // screen.debug();
@@ -77,4 +84,3 @@ test('renders all the book information', async () => {
   expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
   expect(screen.queryByLabelText(/start date/i)).not.toBeInTheDocument()
 })
-
